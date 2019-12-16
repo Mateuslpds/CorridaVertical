c0ac50609565ebda90818458e596de7144d58550
Kauê Luí <ofckaue@gmail.com>
2019-12-16 19:45:36 -0300

    Adicionando alguns métodos

--- race/game.js
+++ race/game.js
@@ -15,5 +15,25 @@ let game = new Vue({
     setInterval(this.loop, 1000 / this.frequency)
     this.obstacle.x = this.obstacle.carPosition[Math.floor(Math.random()*this.obstacle.carPosition.length)]
   },
+  methods: {
+    keyup: function(evt) {
+      this.player.x = this.player.x
+    },
+    keydown: function(evt) {
+      if (evt.keyCode == 37 && this.playerposition > 0) {
+        this.player.x -= 170
+        this.playerposition--
+      }
+      else if (evt.keyCode == 39 && this.playerposition < 3) {
+        this.player.x += 170
+        this.playerposition++
+      }
+    },
+    gameLogic() {
 
-})
+     $('#game').animate({
+       'background-position-y': '-200000%'
+     }, 2000000, 'linear');
+
+   },
+ })
